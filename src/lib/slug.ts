dbefbae9e8404c5d7b3ba070f8355626eeb55275
cucnau <cucnau01@gmail.com>
2026-08-18 23:42:35 +0700

    Add slugify function to convert text to slugs

---
+++ src/lib/slug.ts
@@ -0,0 +1,13 @@
+export function slugify(text: string): string {
+  if (!text) return '';
+  return text
+    .toString()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[đĐ]/g, 'd')
+    .replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
